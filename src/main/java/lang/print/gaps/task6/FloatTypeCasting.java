ce2938b667c38025c354324b69518550c4b37a40
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-15 09:51:43 +0200

    Update FloatTypeCasting.java

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded);
+    }
+    public static void main(String[] args) {
+     FloatTypeCasting main = new FloatTypeCasting();
+        main.roundNumber(10.6f);
     }
 }
